c96a762801ab183aa74ab8190063939d498c1727
Ken Sodemann <ken@ionic.io>
2021-01-11 16:47:11 -0600

    feat(yoda): create the base app

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, ViewChild } from '@angular/core';
+import { ChatMessage } from '@app/models';
+import { ChatService } from '@app/core';
 
 @Component({
   selector: 'app-home',
@@ -6,7 +8,18 @@ import { Component } from '@angular/core';
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+  message: string;
+  messages: Array<ChatMessage>;
+  @ViewChild('messageInput') inp: HTMLIonInputElement;
 
-  constructor() {}
+  constructor(private chat: ChatService) {}
 
+  sendMessage(evt: Event) {
+    this.inp.setFocus();
+    if (this.message) {
+      this.chat.send(this.message);
+      this.messages = this.chat.history();
+      this.message = '';
+    }
+  }
 }
