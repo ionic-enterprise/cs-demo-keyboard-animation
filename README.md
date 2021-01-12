4b48d9098df7b7491030ecd2683cfd48f2640e15
Ken Sodemann <ken@ionic.io>
2021-01-11 18:39:01 -0600

    feat(keyboard): animate keyboard on iOS

---
+++ README.md
@@ -0,0 +1,22 @@
+# Demo Keyboard Animation
+
+Show how to use certain keyboard events in order to improve the UX when displaying the keyboard on iOS.
+
+For reference of what is happening here, have a look at our <a href="https://ionicframework.com/blog/keyboard-improvements-for-ionic-apps/" target="_blank">keyboard blog post</a>. Specifically refer to the part pertaining to Ionic Keyboard Events.
+
+## Running the App
+
+- clone the repo
+- cd into the repo
+- `npm i`
+- `npm run build`
+- `npx cap sync`
+- `npx cap open ios`
+
+Run it on your favorite iOS device.
+
+When you click in the input to chat with Yoda, the animations involved with adjusting to the keyboard being displayed should be nice and smooth.
+
+## Code
+
+The interesting code is in the HomePage class. Have a look at it.
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,4 +1,7 @@
-import { Component, ViewChild } from '@angular/core';
+import { Component, ViewChild, OnInit, OnDestroy } from '@angular/core';
+import { KeyboardInfo, Plugins, KeyboardResize } from '@capacitor/core';
+import { Platform } from '@ionic/angular';
+
 import { ChatMessage } from '@app/models';
 import { ChatService } from '@app/core';
 
@@ -7,12 +10,30 @@ import { ChatService } from '@app/core';
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
-export class HomePage {
+export class HomePage implements OnInit, OnDestroy {
   message: string;
   messages: Array<ChatMessage>;
-  @ViewChild('messageInput') inp: HTMLIonInputElement;
 
-  constructor(private chat: ChatService) {}
+  @ViewChild('messageInput') inp: HTMLIonInputElement;
+  @ViewChild('messageFooter') footer: HTMLIonFooterElement;
+
+  constructor(private chat: ChatService, private platform: Platform) {}
+
+  ngOnInit() {
+    if (this.platform.is('ios')) {
+      const { Keyboard } = Plugins;
+      Keyboard.setResizeMode({ mode: KeyboardResize.None });
+      Keyboard.addListener('keyboardWillShow', (info: KeyboardInfo) =>
+        this.animateIn(info.keyboardHeight),
+      );
+      Keyboard.addListener('keyboardWillHide', () => this.animateOut());
+    }
+  }
+
+  ngOnDestroy() {
+    const { Keyboard } = Plugins;
+    Keyboard.removeAllListeners();
+  }
 
   sendMessage(evt: Event) {
     this.inp.setFocus();
@@ -21,5 +42,24 @@ export class HomePage {
       this.messages = this.chat.history();
       this.message = '';
     }
+  }
+
+  private animateIn(keyboardHeight: number) {
+    const footerHtmlElement = <HTMLElement>(<any>this.footer).el;
+    const toolbarHtmlElement = footerHtmlElement.querySelector('ion-toolbar');
+    footerHtmlElement.style.setProperty(
+      'transform',
+      `translate3d(0, -${keyboardHeight}px, 0)`,
+    );
+    footerHtmlElement.style.transition =
+      'transform 0.3s cubic-bezier(0.1, 0.76, 0.55, 0.9)';
+    toolbarHtmlElement.style.paddingBottom = '0px';
+  }
+
+  private animateOut() {
+    const footerHtmlElement = <HTMLElement>(<any>this.footer).el;
+    const toolbarHtmlElement = footerHtmlElement.querySelector('ion-toolbar');
+    footerHtmlElement.style.removeProperty('transform');
+    toolbarHtmlElement.style.paddingBottom = 'var(--ion-safe-area-bottom, 0)';
   }
 }
